ed228df7be993ab4724aa77854c8e2aeb04fa20d
Nikita <85439842+Easymaki@users.noreply.github.com>
2022-03-21 17:13:23 +0400

    Update func.php

--- app/addons/history_order/func.php
+++ app/addons/history_order/func.php
@@ -1,40 +1,26 @@
 <?php
 
-if (!defined('BOOTSTRAP')) { die('Access denied'); }
+if (!defined('BOOTSTRAP')) { die('Access denied');}
 
 function fn_history_order_change_order_status($status_to, $status_from, $order_info, $force_notification, $order_statuses, $place_order)
 {
     $order_data = array(
         'user_id' => $_SESSION['auth']['user_id'],
+        'order_id' => $order_info['order_id'],
         'status_from' => $status_from,
         'status_to' => $status_to,
-        'updated' => $order_info['updated_at'],
+        'timestamp' => $order_info['updated_at'],
     );
 
-    $data = array($order_data);
-
-    $old_history = db_get_field('SELECT history FROM ?:orders WHERE order_id = ?i', $order_info['order_id']);
-    if (!empty($old_history)) {
-        $prev_data = unserialize($old_history);
-
-        foreach ($prev_data as $item) {
-            array_push($data, $item);
-        }
-
-        db_query("UPDATE ?:orders SET history = ?s WHERE order_id = ?i", serialize($data), $order_info['order_id']);
-    } else {
-        db_query("UPDATE ?:orders SET history = ?s WHERE order_id = ?i", serialize($data), $order_info['order_id']);
-    }
+    db_query('INSERT INTO ?:history_orders ?e', $order_data);
 }
 
 function fn_history_order_get_order_info(&$order, $additional_data)
-{
-    $history = db_get_field('SELECT history FROM ?:orders WHERE order_id = ?i', $order['order_id']);
-    $order['history'] = unserialize($history);
-
-    foreach ($order['history'] as &$item) {
-        $item['status_from_descr'] = fn_get_status_data($item['status_from'], 'O');
-        $item['status_to_descr'] = fn_get_status_data($item['status_to'], 'O');
-        $item['user_info'] = fn_get_user_info($item['user_id']);
+{   
+    if (!empty($order)) {
+        $order['history'] = db_get_array('SELECT ?:history_orders.status_from, ?:history_orders.status_to, ?:users.firstname, ?:users.lastname FROM ?:history_orders
+        LEFT JOIN ?:users
+        ON ?:history_orders.user_id = ?:users.user_id
+        WHERE ?:history_orders.order_id = ?i', $order['order_id']);
     }
 }
