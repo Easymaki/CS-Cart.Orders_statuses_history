47937ba65c3dc0176f1b061e91639861bf7515ad
Nikita <85439842+Easymaki@users.noreply.github.com>
2022-06-01 11:26:35 +0400

    Update func.php

--- app/addons/history_order/func.php
+++ app/addons/history_order/func.php
@@ -20,7 +20,7 @@ function fn_history_order_change_order_status($status_to, $status_from, $order_i
 function fn_history_order_get_order_info(&$order, $additional_data)
 {   
     if (!empty($order)) {
-        $order['history'] = db_get_array('SELECT ?:history_orders.status_from, ?:history_orders.status_to, ?:users.firstname, ?:users.lastname FROM ?:history_orders
+        $order['history'] = db_get_array('SELECT ?:history_orders.user_id, ?:history_orders.status_from, ?:history_orders.status_to, ?:users.firstname, ?:users.lastname FROM ?:history_orders
         LEFT JOIN ?:users
         ON ?:history_orders.user_id = ?:users.user_id
         WHERE ?:history_orders.order_id = ?i', $order['order_id']);
